649c76f804997b632181194f459876d1c11c09a3
rejecting-ambulance <zhandezhonghenry@gmail.com>
2026-01-26 14:31:20 +0800

    Import Service from selenium.webdriver.chrome.service

--- core.py
+++ core.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 
